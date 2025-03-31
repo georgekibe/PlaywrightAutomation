4b5888b6660411594b02d4bda6acd25b5cad58ad
georgekibe <georgekibe500@gmail.com>
2025-03-31 23:24:39 +0300

    Updating tests

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,9 +1,12 @@
 
-const { test }  = require ('@playwright/test')
+const { test, expect } = require('@playwright/test')
 
 
-test ("Browser Load Page", async ({page}) => {
-await page.goto("https://rahulshettyacademy.com/locatorspractice/")
+test("Browser Load Page", async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/locatorspractice/")
+
+    console.log(await page.title())
+    await expect(page).toHaveTitle('Rahul Shetty Academy - Login page')
 });
 
 
